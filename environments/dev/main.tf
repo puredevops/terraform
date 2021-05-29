2016a6ff74247f87c91765266b8c9d2354423bf1
Samaya Ramesh Radhakrishnan <samayaramesh.r@Samayas-MBP.bbrouter>
2021-05-29 22:23:30 +0530

    added vpc module

---
+++ environments/dev/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    region  = "eu-west-1"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+  azs             = ["eu-west-1a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+  enable_nat_gateway = true
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
